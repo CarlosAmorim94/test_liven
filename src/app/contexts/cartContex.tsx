1c0a52dafa219f080f83570f0a38554c2137dd31
Carlos <carlos.amorim@psicomanager.com.br>
2024-08-31 10:49:57 -0300

    Feat: Created contex

---
+++ src/app/contexts/cartContex.tsx
@@ -0,0 +1,62 @@
+import { createContext, useContext, useState, ReactNode } from "react"
+import { IProduct } from "@/types/fakeStoreTypes"
+
+interface CartContextType {
+  cart: { [key: number]: { product: IProduct; quantity: number } }
+  addToCart: (product: IProduct, quantity: number) => void
+  removeFromCart: (id: number) => void
+  updateQuantity: (id: number, quantity: number) => void
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined)
+
+export const CartProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [cart, setCart] = useState<{
+    [key: number]: { product: IProduct; quantity: number }
+  }>({})
+
+  const addToCart = (product: IProduct, quantity: number) => {
+    setCart((prevCart) => ({
+      ...prevCart,
+      [product.id]: {
+        product,
+        quantity: (prevCart[product.id]?.quantity || 0) + quantity,
+      },
+    }))
+  }
+
+  const removeFromCart = (id: number) => {
+    setCart((prevCart) => {
+      const { [id]: _, ...rest } = prevCart
+      return rest
+    })
+  }
+
+  const updateQuantity = (id: number, quantity: number) => {
+    setCart((prevCart) => ({
+      ...prevCart,
+      [id]: {
+        ...prevCart[id],
+        quantity,
+      },
+    }))
+  }
+
+  return (
+    <CartContext.Provider
+      value={{ cart, addToCart, removeFromCart, updateQuantity }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export const useCart = () => {
+  const context = useContext(CartContext)
+  if (context === undefined) {
+    throw new Error("useCart must be used within a CartProvider")
+  }
+  return context
+}
--- src/app/providers/ClientWrapper.tsx
+++ src/app/providers/ClientWrapper.tsx
@@ -2,11 +2,14 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
+import { CartProvider } from "../contexts/cartContex"
 
 const queryClient = new QueryClient()
 
 export const ClientWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <CartProvider>{children}</CartProvider>
+    </QueryClientProvider>
   )
 }
