84c800731afba4a557d4062b6f13d33c9477d2b7
Carlos <carlos.amorim@psicomanager.com.br>
2024-09-01 14:35:31 -0300

    Feat: Created QuantityButtons components

---
+++ src/app/components/quantityButtons/quantityButtons.tsx
@@ -0,0 +1,31 @@
+import { FC } from "react"
+
+type IQuantityButtonsProps = {
+  onClickButtonAdd: () => void
+  onClickButtonRemove: () => void
+  quantity: number
+}
+
+export const QuantityButtons: FC<IQuantityButtonsProps> = ({
+  onClickButtonAdd,
+  onClickButtonRemove,
+  quantity,
+}) => {
+  return (
+    <div className="flex items-center space-x-2 mb-4">
+      <button
+        onClick={() => onClickButtonRemove()}
+        className="px-2 py-1 bg-gray-200 rounded"
+      >
+        -
+      </button>
+      <span>{quantity}</span>
+      <button
+        onClick={() => onClickButtonAdd()}
+        className="px-2 py-1 bg-gray-200 rounded"
+      >
+        +
+      </button>
+    </div>
+  )
+}
